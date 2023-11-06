5a79e01185a491fdd2f6c5b865f3c4425d8d809b
pistomat <pistomat@users.noreply.github.com>
2023-11-06 11:12:03 +0100

    Reimplement with InvokeFunction

--- rpc_state_reader/tests/multicall.rs
+++ rpc_state_reader/tests/multicall.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
 use cairo_vm::felt::{felt_str, Felt252};
-use rpc_state_reader::rpc_state::{BlockValue, RpcChain, RpcState};
+use rpc_state_reader::rpc_state::{BlockValue, RpcBlockInfo, RpcChain, RpcState};
 use starknet_api::{
     block::BlockNumber,
     core::{ClassHash as SNClassHash, ContractAddress, PatriciaKey},
@@ -10,7 +10,14 @@ use starknet_api::{
 };
 use starknet_in_rust::{
     core::errors::state_errors::StateError,
-    definitions::block_context::BlockContext,
+    definitions::{
+        block_context::{BlockContext, StarknetChainId, StarknetOsConfig},
+        constants::{
+            DEFAULT_CAIRO_RESOURCE_FEE_WEIGHTS, DEFAULT_CONTRACT_STORAGE_COMMITMENT_TREE_HEIGHT,
+            DEFAULT_GLOBAL_STATE_COMMITMENT_TREE_HEIGHT, DEFAULT_INVOKE_TX_MAX_N_STEPS,
+            DEFAULT_VALIDATE_MAX_N_STEPS,
+        },
+    },
     execution::{
         execution_entry_point::ExecutionEntryPoint, CallType, TransactionExecutionContext,
     },
@@ -19,8 +26,9 @@ use starknet_in_rust::{
         cached_state::{CachedState, ContractClassCache},
         state_api::StateReader,
         state_cache::StorageEntry,
-        ExecutionResourcesManager,
+        BlockInfo, ExecutionResourcesManager,
     },
+    transaction::{InvokeFunction, Transaction},
     utils::{calculate_sn_keccak, Address, ClassHash},
     EntryPointType,
 };
@@ -81,11 +89,47 @@ fn test_multicall() {
     // An attempt to recreate the execution of the following transaction:
     // https://starkscan.co/tx/0x036951b882702c3e5ce97a0f43f473edfc97c405471f6ecb34e54e47c0898130
 
-    let block_number = 373172; // The actual block number is 373173, but we need to simulate on top of the previous block
-    let block = BlockValue::Number(BlockNumber(block_number));
+    let fee_token_address = Address(felt_str!(
+        "049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
+        16
+    ));
+
+    let block_number = BlockNumber(373172); // The actual block number is 373173, but we need to simulate on top of the previous block
+    let block = BlockValue::Number(block_number);
     let chain = RpcChain::MainNet;
 
+    // Create the state reader
     let rpc_state_reader = RpcStateReader(RpcState::new_infura(chain, block));
+    let gas_price = rpc_state_reader.0.get_gas_price(block_number.0).unwrap();
+
+    // Get values for block context before giving ownership of the reader
+    let chain_id = match rpc_state_reader.0.chain {
+        RpcChain::MainNet => StarknetChainId::MainNet,
+        RpcChain::TestNet => StarknetChainId::TestNet,
+        RpcChain::TestNet2 => StarknetChainId::TestNet2,
+    };
+    let starknet_os_config =
+        StarknetOsConfig::new(chain_id.to_felt(), fee_token_address, gas_price);
+    let block_info = {
+        let RpcBlockInfo {
+            block_number,
+            block_timestamp,
+            sequencer_address,
+            ..
+        } = rpc_state_reader.0.get_block_info();
+
+        let block_number = block_number.0;
+        let block_timestamp = block_timestamp.0;
+        let sequencer_address = Address(Felt252::from_bytes_be(sequencer_address.0.key().bytes()));
+
+        BlockInfo {
+            block_number,
+            block_timestamp,
+            gas_price,
+            sequencer_address,
+        }
+    };
+
     let class_cache = ContractClassCache::default();
     let mut state = CachedState::new(Arc::new(rpc_state_reader), class_cache);
 
@@ -103,7 +147,7 @@ fn test_multicall() {
         16
     );
 
-    let call_data = vec![
+    let calldata = vec![
         felt_str!("2"),
         felt_str!("2368576823837625528275935341135881659748932889268308403712618244410713532584"),
         felt_str!("949021990203918389843157787496164629863144228991510976554585288817234167820"),
@@ -122,39 +166,55 @@ fn test_multicall() {
         felt_str!("0"),
     ];
 
-    let entry_point = ExecutionEntryPoint::new(
-        contract_address,
-        call_data,
-        entry_point_selector,
-        caller_address,
-        EntryPointType::External,
-        CallType::Delegate.into(),
-        class_hash.to_be_bytes().into(),
-        u128::MAX,
-    );
+    // let entry_point = ExecutionEntryPoint::new(
+    //     contract_address,
+    //     call_data,
+    //     entry_point_selector,
+    //     caller_address,
+    //     EntryPointType::External,
+    //     CallType::Call.into(),
+    //     class_hash.to_be_bytes().into(),
+    //     // None,
+    //     u128::MAX,
+    // );
+    // dbg!(&entry_point);
 
-    dbg!(&entry_point);
+    let tx = Transaction::InvokeFunction(
+        InvokeFunction::new(
+            contract_address,
+            entry_point_selector,
+            1000000,
+            1.into(),
+            calldata,
+            vec![],
+            StarknetChainId::MainNet.to_felt(),
+            Some(17.into()),
+        )
+        .unwrap(),
+    );
 
     // Set up the call context
-    let block_context = BlockContext::default();
-    let mut resources_manager = ExecutionResourcesManager::default();
-    let mut tx_execution_context = TransactionExecutionContext::create_for_testing(
-        Address(0.into()),
-        10,
-        0.into(),
-        block_context.invoke_tx_max_n_steps(),
-        2.into(),
+    let block_context = BlockContext::new(
+        starknet_os_config,
+        DEFAULT_CONTRACT_STORAGE_COMMITMENT_TREE_HEIGHT,
+        DEFAULT_GLOBAL_STATE_COMMITMENT_TREE_HEIGHT,
+        DEFAULT_CAIRO_RESOURCE_FEE_WEIGHTS.clone(),
+        DEFAULT_INVOKE_TX_MAX_N_STEPS,
+        DEFAULT_VALIDATE_MAX_N_STEPS,
+        block_info,
+        Default::default(),
+        true,
     );
 
-    // Execute the simulated call
-    let result = entry_point.execute(
-        &mut state,
-        &block_context,
-        &mut resources_manager,
-        &mut tx_execution_context,
+    // Execute the call
+    let simulation_tx = tx.create_for_simulation(
         true,
-        block_context.invoke_tx_max_n_steps(),
+        false,
+        true,
+        true,
+        true,
     );
+    let result = simulation_tx.execute(&mut state, &block_context, u128::MAX);
     dbg!(&result);
 
     assert!(result.is_ok());
